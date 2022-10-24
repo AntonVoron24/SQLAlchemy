88556895d1640391d76fcb982f280e8da047e32b
AntonVoron24 <lovemoto24@mail.ru>
2022-10-24 22:34:17 +0300

    refactor def

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ with open('tests_data.json', encoding='utf-8') as f:
         session.close()
 
 
-def get_publisher():
+def get_shops():
     search = input('Введите имя или идентификатор издателя --> ')
-
+    shops = session.query(Shop).join(Stock.shop).join(Book).join(Publisher)
     if search.isnumeric():
-        for result in session.query(Publisher).filter(Publisher.id == int(search)).all():
+        for result in shops.filter(Publisher.id == search).all():
             print(result)
     else:
-        for result in session.query(Publisher).filter(Publisher.name.like(f'%{search}%')).all():
+        for result in shops.filter(Publisher.name.like(f'%{search}%')).all():
             print(result)
 
 
-get_publisher()
+get_shops()
